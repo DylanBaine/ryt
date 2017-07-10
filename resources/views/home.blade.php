f0952e4f5bce9871b7d9d66059aa1ea00f48f91b
Dylan Baine <dylan.baine@yahoo.com>
2017-07-10 13:02:23 -0500

    add reviews and star rating

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-admin home
+<div class="container-fluid">
+	<div class="row">
+		<aside class="col-md-6 col-md-offset-3">
+			<form action="/upload-db" method="post" enctype="multipart/form-data">
+				{{csrf_field()}}
+				<div class="form-group">
+					<label for="db-upload" class="btn btn-transparent btn-block">Upload db files</label>
+					<br>
+					<input type="file" id="db-upload" name="db_file" style="display: block;">
+				</div>
+				<input type="submit" class="btn btn-primary btn-block" value="Save">
+			</form>
+		</aside>
+	</div>
+</div>
 @endsection
